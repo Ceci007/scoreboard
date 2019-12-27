07d8497c3881f9da2a470698dc6e2b75f72f99c4
PequeCeci <hey.ceci.lets.go@gmail.com>
2019-12-27 14:29:50 -0400

    Updates

---
+++ precache-manifest.cb062af7e4ef08cdc5ef2b3aa09e158f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625af67d44946e713c92b8f19f816320",
+    "url": "/scoreboard/index.html"
+  },
+  {
+    "revision": "d6ac09267e02b82292c5",
+    "url": "/scoreboard/static/css/main.140df8a8.chunk.css"
+  },
+  {
+    "revision": "25ea686f562b20bab6ea",
+    "url": "/scoreboard/static/js/2.f0ef8b63.chunk.js"
+  },
+  {
+    "revision": "d1fdb105e50f33eda8cdf2224c3a538c",
+    "url": "/scoreboard/static/js/2.f0ef8b63.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d6ac09267e02b82292c5",
+    "url": "/scoreboard/static/js/main.cdc81c20.chunk.js"
+  },
+  {
+    "revision": "79d1ee12da2debc7d41e",
+    "url": "/scoreboard/static/js/runtime-main.a5384c14.js"
+  }
+]);
